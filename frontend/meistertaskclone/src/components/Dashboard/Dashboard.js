7fe43684f8fb7077db7d1b90232f914b33e5dd27
Logan Phillips <loganphillips792@gmail.com>
2021-04-05 20:40:04 -0400

    Add file for Dashboard, create route

---
+++ frontend/meistertaskclone/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div``;
+
+const Dashboard = () => {
+
+    return (
+        <Container>
+            <h1>Dashboard</h1>
+        </Container>
+    );
+}
+
+export default Dashboard;
