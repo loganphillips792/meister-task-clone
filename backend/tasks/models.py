99873098a8eba470478b27f7cfb172151594cd5a
Logan Phillips <loganphillips792@gmail.com>
2021-04-07 20:26:29 -0400

    Model changes, database script

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 import uuid
+from profile.models import Profile
 
 class Section(models.Model):
     name = models.CharField(max_length=30)
-    color = models.CharField(max_length=6)
+    color = models.CharField(max_length=7)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -12,9 +13,33 @@ class Section(models.Model):
 
 class Task(models.Model):
     #id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=250)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    due = models.DateField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    completed_at = models.DateTimeField(auto_now=True, null=True)
+    assigned_to_id = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f"Task ID: {self.id}"
 
     class Meta:
         db_table="tasks"
+
+class Note(models.Model):
+    task = models.ForeignKey(
+        Task, 
+        on_delete=models.CASCADE, 
+        null=True
+    )
+
+    content = models.CharField(max_length=1000)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Note ID: {self.id}"
+
+    class Meta:
+        db_table="notes"
