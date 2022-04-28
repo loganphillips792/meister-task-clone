290eab6f0feeaff5028e3d4518fc24aeaeca751e
loganphillips792 <loganphillips792@gmail.com>
2022-04-28 00:39:42 -0400

    frontend, backend

--- backend/main.go
+++ backend/main.go
@@ -1,24 +1,62 @@
 package main
 
 import (
+	"cors"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
+	"zap"
 )
 
-type FileInfo struct {
-	Name string
+type Handler struct {
+	logger *zap.SugaredLogger
+	//dbConn *sql.DB
 }
 
 func main() {
 	fmt.Println("Hello world")
 
-	http.HandleFunc("/hello", HelloWorld)
+	logger, _ := zap.NewProduction()
+	defer logger.Sync() // flushes buffer, if any
+	sugar := logger.Sugar()
 
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	envHandler := &Handler{logger: sugar}
+	//envHandler := &Handler{logger: sugar, dbConn: db}
+
+	c := cors.New(cors.Options{
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE"},
+	})
+
+	http.HandleFunc("/hello", envHandler.HelloWorld)
+	http.HandleFunc("/random", envHandler.GetRandomString)
+
+	handler := c.Handler()
+
+	log.Fatal(http.ListenAndServe(":8000", handler))
 }
 
-func HelloWorld(w http.ResponseWriter, req *http.Request) {
+func (handler *Handler) HelloWorld(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(`{"status":"OK"}`))
+}
+
+func (handler *Handler) GetRandomString(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	data := fmt.Sprintf("{response:%s}", randStringRunes(10))
+	w.Write([]byte(data))
+	//w.Write([]byte(`{"status":"OK"}`))
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randStringRunes(n int) string {
+	rand.Seed(time.Now().UnixNano())
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled, { createGlobalStyle } from 'styled-components';
+import variables from './variables';
+
+const Container = styled.div`
+  height: 100%;
+`;
+
+const GlobalStyle = createGlobalStyle`
+  ${variables}
+  body {
+    background-color: var(--dark-color);
+  }
+`;
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/random")
+        .then(response => response.json())
+        .then(data => {
+            console.log("DATA", data)
+            setData(data);
+        })
+}, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <GlobalStyle />
+      <h1>Response from server: {data}</h1>
+    </Container>
   );
 }
 
