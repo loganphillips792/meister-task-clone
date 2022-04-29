a7241d0e63743d528c9e4b626951aacfa28e88e8
loganphillips792 <loganphillips792@gmail.com>
2022-04-28 22:24:10 -0400

    fix linting

--- backend/main.go
+++ backend/main.go
@@ -26,7 +26,13 @@ func main() {
 	fmt.Println("Hello world")
 
 	logger, _ := zap.NewProduction()
-	defer logger.Sync() // flushes buffer, if any
+
+	//logger.Sync() // flushes buffer, if any
+	err := logger.Sync() // flushes buffer, if any
+	// for linting
+	if err != nil {
+		log.Fatal("Error when encoding json")
+	}
 
 	sugar := logger.Sugar()
 
@@ -50,6 +56,8 @@ func main() {
 func (handler *Handler) HelloWorld(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	_, err := w.Write([]byte(`{"status":"OK"}`))
+
+	// for linting
 	if err != nil {
 		handler.logger.Fatalf("Error when writing")
 	}
@@ -61,6 +69,7 @@ func (handler *Handler) GetRandomString(w http.ResponseWriter, req *http.Request
 		RandString: randStringRunes(10),
 	})
 
+	// for linting
 	if err != nil {
 		handler.logger.Fatalf("Error when encoding json")
 	}
